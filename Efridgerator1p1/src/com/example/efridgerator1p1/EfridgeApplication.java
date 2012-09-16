0843615c738ad2fc98d1712c149d6510ee901f80
at <at507@yahoo.com>
2012-09-16 02:06:06 -0400

    // at issue #8 : 9/16/2012 // Create database and handle queries // Application class created for future use of database

---
+++ Efridgerator1p1/src/com/example/efridgerator1p1/EfridgeApplication.java
@@ -0,0 +1,25 @@
+// aat issue #8 : 9/16/2012
+// Create database and handle queries
+// Application class created for future use of database
+
+package com.example.efridgerator1p1;
+
+import android.app.Application;
+
+public class EfridgeApplication extends Application{
+	
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+		
+	}
+
+	@Override
+	public void onTerminate() {
+		// TODO Auto-generated method stub
+		super.onTerminate();
+		
+	}
+	
+}
